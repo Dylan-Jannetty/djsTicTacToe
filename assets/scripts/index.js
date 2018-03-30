eebb752ba9a2fb3264fe0807ecd37578fce19649
dylan-jannetty <djannetty29@gmail.com>
2018-03-30 16:44:52 -0400

    gameEngine
    added functions starting empty game board.
    added function to add users to spot on game board
    added function to switch players

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -8,4 +8,52 @@
 
 $(() => {
   // your JS code goes here
+  // declare an empty gameBoard
+
+  const gameBoard =
+  ['', '', '',
+    '', '', '',
+    '', '', '']
+
+  console.log(gameBoard)
+  // write a function that accepts a x or o and a position to add to board
+  const addToBoard = function (player, position) {
+    gameBoard[position] = player
+  }
+  addToBoard('x', 0)
+  addToBoard('o', 2)
+  addToBoard('x', 3)
+  addToBoard('o', 6)
+  addToBoard('x', 4)
+  addToBoard('o', 7)
+  addToBoard('x', 8)
+  addToBoard('o', 5)
+  addToBoard('x', 1)
+
+  console.log(gameBoard)
+  // write a function that only adds to the board if the spot is free
+  const spotTaken = () => {
+    for (let i = 0; i < gameBoard.length; i++) {
+      if (spotTaken === false && gameBoard[i] === ' ') {
+        console.log('spot is open!')
+      } else {
+        console.log('spot is closed')
+      }
+    }
+  }
+  spotTaken()
+  // Write a function that will switch players
+  // If the current player is "x", then change the current player to "o".
+  // Otherwise, change the current player to "x".
+  let currentPlayer = 'X'
+  const switchTurns = () => {
+    if (currentPlayer === 'X') {
+      currentPlayer = 'O'
+      console.log("it's player ones turn")
+    } else if (currentPlayer === 'O') {
+      currentPlayer = 'X'
+      console.log("it's player twos turn")
+    }
+  }
+  switchTurns()
 })
